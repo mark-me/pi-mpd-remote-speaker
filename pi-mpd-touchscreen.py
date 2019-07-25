bb7bcbc36010b895a67377e3a41451ba29672575
Mark Zwart <mark.zwart@pobox.com>
2019-07-26 00:05:53 +0200

    Added settings.py

--- pi-mpd-touchscreen.py
+++ pi-mpd-touchscreen.py
@@ -22,7 +22,7 @@ __author__ = 'Mark Zwart'
 import sys, pygame
 from time import sleep
 from pygame import *
-# from gui_screens import *
+from gui_screens import *
 # from config_file import *
 # from settings import *
 from mpd_client import *
