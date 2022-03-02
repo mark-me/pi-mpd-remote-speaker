74fed62bb0e8a1a3abe6ba869ecac250378e4114
Mark Zwart <mark.zwart@pobox.com>
2022-03-02 23:51:12 +0100

    __parse_mpc_status add infinite retries

--- run.sh
+++ run.sh
@@ -1,9 +1,22 @@
 #! /bin/bash
 
+COMMAND='python3 pi-mpd-touchscreen.py'
+LOGFILE=restart.txt
+
+writelog() {
+  now=`date`
+  echo "$now $*" >> $LOGFILE
+}
+
 case $SSH_CONNECTION in
   '')
+  writelog "Starting"
   cd pi-mpd-touchscreen
-  python3 pi-mpd-touchscreen.py
+  while true ; do
+    "$COMMAND"
+    writelog "Exited with status $?"
+    writelog "Restarting"
+  done
   cd .. ;;
 esac
 
