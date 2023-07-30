2cb1139a2ffc0fd5a1437910dc75f57ae3f3e686
Mark Zwart <mark.zwart@pobox.com>
2023-07-30 19:06:02 +0200

    Increased transparency title

--- screen_player.py
+++ screen_player.py
@@ -42,7 +42,7 @@ class ScreenPlaying(Screen):
         # Player specific labels
         self.add_component(LabelText('lbl_track_title', self.surface, 0, SCREEN_HEIGHT - 42, SCREEN_WIDTH, 42))
         self.components['lbl_track_title'].set_alignment(HOR_LEFT, VERT_TOP)
-        self.components['lbl_track_title'].background_alpha_set(160)
+        self.components['lbl_track_title'].background_alpha_set(180)
         self.add_component(Slider2('slide_time', self.surface, 0, SCREEN_HEIGHT - 10, SCREEN_WIDTH, 10))
         self.coverart_color = 0
 
