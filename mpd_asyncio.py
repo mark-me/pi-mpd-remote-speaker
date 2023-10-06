4b84d4c63ee5d5e39f5a6ad30e7de407198e3456
Mark Zwart <mark.zwart@pobox.com>
2023-10-06 22:49:14 +0200

    Starting a curses client

--- mpd_asyncio.py
+++ mpd_asyncio.py
@@ -1,12 +1,13 @@
 import asyncio
 import sys
-from tqdm import tqdm
+import curses
+from curses import wrapper
 
 from capture_audio import *
 from mpd_client import *
 
 
-async def main():
+async def main_screen(stdscr):
     audio_spectrometer = AudioSpectrometer()
     task_connect = asyncio.create_task(mpd.connect())
     is_connected = await task_connect
@@ -15,30 +16,31 @@ async def main():
             mpd.port) + "! Check settings in file pi-jukebox.conf or check is server is running 'systemctl status mpd'.")
         sys.exit()
 
-    pbar = tqdm(total=100, desc="Song progress: ")
-    abar = tqdm(total=1000, desc="Amplitude: ")
     while True:
+        stdscr.clear()
         task_get_status = asyncio.create_task(mpd.status_get())
         if await task_get_status:
-            print(mpd.now_playing.title)
+            stdscr.addstr(10, 10, mpd.now_playing.title)
         try:
             event = mpd.events.popleft()
             playing = mpd.now_playing
             if event == 'time_elapsed':
                 #pbar.update(playing.time_percentage)
-                print("Time elapsed: " + str(playing.time_percentage))
+                stdscr.addstr(12, 10, "Time elapsed: " + str(playing.time_percentage))
             if event == 'playing_file':
-                print('Title: ' + mpd.now_playing.title)
-                print('Artist: ' + mpd.now_playing.artist)
+                stdscr.addstr(14, 10, 'Title: ' + mpd.now_playing.title)
+                stdscr.addstr(15, 10, 'Artist: ' + mpd.now_playing.artist)
             if event == 'album_change':
                 file_img_cover = await mpd.now_playing.get_cover_art()
-                print('Cover file: ' + file_img_cover)
+                #print('Cover file: ' + file_img_cover)
         except IndexError:
             pass
         amplitude = round(audio_spectrometer.listen())
-        # abar.update(amplitude)
-        print("Amplitude: " + str(amplitude))
 
+        stdscr.addstr(17, 10, "Amplitude: " + str(amplitude))
+
+def main(stdscr) -> None:
+    return asyncio.run(main_screen(stdscr=stdscr))
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    wrapper(main)
